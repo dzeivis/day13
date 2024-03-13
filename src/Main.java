5f142fa4fd57420ef74d3816b4f497acc2db5259
dzeivis <deividaspapuska@gmail.com>
2024-03-13 15:35:12 +0200

    conflict branch Main code changes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Labas pasauli!");
+        System.out.println("Helloa world!");
+        System.out.println("Labas pasauly!");
     }
 }
